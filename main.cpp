8622e3c01ac53bdbbb930c8726336024d62d0042
Infromatica II <eder.ariza@udea.edu.co>
2020-10-06 02:55:04 -0500

    Ejercicio 26 Laboratorio 1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+/*
+Ejercicio 26. Escriba un programa que pida tres números e imprima el tipo de triangulo (isósceles, equilátero, escaleno) que se formaría, si sus lados tienen la longitud denida por los números
+ingresados. Tenga en cuenta el caso en que los números ingresados no forman un triángulo.
+Ej: si se ingresan 3, 3 y 5 se debe imprimir:
+Se forma un triangulo isosceles.
+y si se ingresan 3, 3 y 6 se debe imprimir:
+Las longitudes ingresadas no forman un triangulo
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
